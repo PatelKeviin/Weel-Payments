5fa7c94d989f4c9b20e5c0033a8fed64feeea7fb
Kevin Patel <kevin.patel@socoro.com.au>
2023-12-28 00:50:43 +1100

    cards app models implemented

--- weel_payments/cards/models.py
+++ weel_payments/cards/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.db.models.functions import Now
+
 
 # Create your models here.
+class Card(models.Model):
+    id = models.AutoField(primary_key=True)
+    number = models.CharField(max_length=16, unique=True)
+    exp_date = models.DateField("expiry date")
+    cvc_code = models.CharField("security code", max_length=3)
+    owner_name = models.CharField(max_length=100)
+    balance = models.DecimalField(max_digits=12, decimal_places=2)
+    active = models.BooleanField(default=True)
+
+
+class CardControl(models.Model):
+    CATEGORY = "CATG"
+    MERCHANT = "MCHT"
+    MAX_AMOUNT = "MXAM"
+    MIN_AMOUNT = "MNAM"
+    CATEGORY_TYPE = {
+        CATEGORY: "Category",
+        MERCHANT: "Merchent",
+        MAX_AMOUNT: "Max Amount",
+        MIN_AMOUNT: "Min Amount",
+    }
+    id = models.AutoField(primary_key=True)
+    card = models.ForeignKey(Card, on_delete=models.CASCADE)
+    type = models.CharField(
+        max_length=4,
+        choices=CATEGORY_TYPE,
+        default=CATEGORY,
+    )
+    value = models.CharField(max_length=30)
+    active = models.BooleanField(default=True)
+
+
+class Transaction(models.Model):
+    APPROVED = "APPR"
+    DECLINED = "DEC"
+    STATUS = {APPROVED: "Approved", DECLINED: "Declined"}
+    id = models.AutoField(primary_key=True)
+    card = models.ForeignKey(Card, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField("creation timestamp", db_default=Now())
+    amount = models.DecimalField(max_digits=12, decimal_places=2)
+    status = models.CharField(
+        max_length=4,
+        choices=STATUS,
+        default=APPROVED,
+    )
+    message = models.CharField(max_length=100, default="")
